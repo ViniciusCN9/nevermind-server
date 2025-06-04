7db5bd63bad3128575dd74407ae7888ef875ef0d
ViniciusCN9 <vinicius.engenhariacn@gmail.com>
2025-06-04 02:55:39 -0300

    Adiciona JAR

--- builds/run.bat
+++ builds/run.bat
@@ -1,7 +1,8 @@
 @echo off
-for /f "delims=" %%i in ('powershell -Command "[guid]::NewGuid().ToString()"') do set "JWT_SECRET_KEY=%%i"
+for /f "delims=" %%i in ('powershell -NoProfile -Command "[Convert]::ToBase64String(([guid]::NewGuid()).ToByteArray())"') do set "JWT_SECRET_KEY=%%i"
 
 echo JWT_SECRET_KEY=%JWT_SECRET_KEY%
 
 java -DJWT_SECRET_KEY=%JWT_SECRET_KEY% -jar server-0.0.1-SNAPSHOT.jar
 pause
+
